e7c43e80ef4d090a66b5a13d682c1e66a5745af6
Charles Lai <cjl223@cornell.edu>
2016-02-15 20:55:32 -0500

    clean up make game script

--- make_game.sh
+++ make_game.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Usage: ./make_game.sh -c CompanyName -g GameName
+
+# Set default variable values
 GAME=MyGame
 COMPANY=MyCompany
 
+# Process CLI values
 while [[ $# > 1 ]]
 do
 key="$1"
@@ -23,6 +26,7 @@ esac
 shift 
 done
 
+# Main function
 echo "Creating game ${GAME} with company ${COMPANY}..."
 cocos new ${GAME} -p com.${COMPANY}.${GAME} -l cpp -d ./${COMPANY}
 
